d2a4e08c99639e85cb82eb92eb01829454afd0bd
Shivraj Singh Kushwah <shivraj2726@gmail.com>
2026-08-07 02:48:18 +0530

    Change backend settings Debug for Deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1vs=ji5vs32w-2%ozn@o+pd@8ke_vcvo)dxf11yqw!b6p0as-j')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
