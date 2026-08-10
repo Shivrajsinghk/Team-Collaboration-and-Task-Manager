078e33448ad9473158288ba970d7af3745e57f26
Shivraj Singh Kushwah <shivraj2726@gmail.com>
2026-08-10 15:32:52 +0530

    Add some comments in the Dockerfile as well as Nginx configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -167,4 +167,8 @@ STORAGES = {
     },
 }
 
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
+CSRF_TRUSTED_ORIGINS = (
+    os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') 
+    if os.environ.get('CSRF_TRUSTED_ORIGINS') 
+    else []
+)
